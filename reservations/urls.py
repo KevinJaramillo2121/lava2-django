69c5483623be1bf7c5d0ce738ea86a0a1527ac71
KevinJaramillo2121 <kj455955@gmail.com>
2025-09-19 10:02:37 -0500

    montaje de las urls de reservaciones

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CrearReservaView
+from .views import CrearReservaView, get_empleados_por_sede
 
 app_name = 'reservations' # Esto es importante para el namespacing
 
 urlpatterns = [
     path('crear/', CrearReservaView.as_view(), name='crear_reserva'),
     # En el futuro: path('resumen/<int:pk>/', ResumenReservaView.as_view(), name='resumen_reserva'),
+
+    path('api/get-empleados/', get_empleados_por_sede, name='api_get_empleados'),
 ]
